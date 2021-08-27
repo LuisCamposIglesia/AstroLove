e114aaad46b82954b7771f11086e8d8fd90159eb
Luis Campos Iglesia <56022466+LuisCamposIglesia@users.noreply.github.com>
2021-08-27 21:05:35 +0200

    DEPLOY

--- AstroSocial/settings.py
+++ AstroSocial/settings.py
@@ -14,8 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(PROJECT_ROOT)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
